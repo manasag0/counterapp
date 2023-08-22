4649e33574b975e8185aeb375520c28bd546aae6
Manasa G <manasa.g7890@hotmail.com>
2023-08-22 14:16:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [count, setCount] = useState(0);
+  const hadleClickIncrement =()=>{
+    setCount(count +1)
+  }
+  const hadleClickDecrement = () =>{
+    setCount(count-1)
+    if(count<0){
+      setCount()
+    }alert("You cannot decrese more then 0")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+     <button onClick={hadleClickIncrement}>Increse</button>
+     <button onClick={hadleClickDecrement}>Decrese</button>
     </div>
   );
 }
