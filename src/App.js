581342e68dc7861c899e950a7fea32efda158445
Manasa G <manasa.g7890@hotmail.com>
2023-08-22 14:31:46 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ function App() {
     setCount(count +1)
   }
   const hadleClickDecrement = () =>{
-    setCount(count-1)
-    if(count<0){
-      setCount()
-    }alert("You cannot decrese more then 0")
+    setCount(!count-1)
+    if(count <= 0){
+      alert("You can't count more then 0, reset the count")
+    }
+
+  }
+  const handleClickReset = () =>{
+    setCount(0)
+ 
   }
   return (
     <div className="App">
       <h1>{count}</h1>
      <button onClick={hadleClickIncrement}>Increse</button>
+     <button onClick={handleClickReset}>Reset</button>
      <button onClick={hadleClickDecrement}>Decrese</button>
     </div>
   );
